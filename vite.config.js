a0ef52b5f2e6aae691e1c92bb33976db5f424fbd
r4510968-art <r4510968@gmail.com>
2025-10-10 17:31:01 +0530

    log commented

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Replace 'portfolio-manager' with your repo name
 export default defineConfig({
+  base: '/portfolio-manager/',
   plugins: [react()],
 });
